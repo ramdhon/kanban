abf517de96149c6c978c1f33b22c899d9d02882b
ramdhon <ramadhonimuhammad@gmail.com>
2019-04-22 01:11:34 +0700

    done and deployed

--- client/src/components/Form.vue
+++ client/src/components/Form.vue
@@ -1,49 +1,55 @@
 <template>
-  <div id="form">
-    <b-form @submit.prevent="onSubmit" @reset.prevent="onReset" v-if="show">
-      <b-form-group id="input-group-1" label="Title:" label-for="input-1">
-        <b-form-input
-          id="input-1"
-          v-model="form.title"
-          required
-          placeholder="Enter title"
-        ></b-form-input>
-      </b-form-group>
-      <b-form-group id="input-group-2" label="Description:" label-for="textarea ">
-        <b-form-textarea
-          id="textarea"
-          v-model="form.description"
-          placeholder="Enter description"
-          rows="3"
-          max-rows="6"
-        ></b-form-textarea>
-      </b-form-group>
-      <b-form-group id="input-group-3" label="Point:" label-for="input-3">
-        <b-form-input
-          id="input-3"
-          v-model="form.point"
-          placeholder="Enter point"
-        ></b-form-input>
-      </b-form-group>
-      <b-form-group id="input-group-4" label="PIC:" label-for="input-4">
-        <b-form-input
-          id="input-4"
-          v-model="form.pic"
-          placeholder="Enter PIC"
-        ></b-form-input>
-      </b-form-group>
+  <b-modal hide-footer :id="formId" centered title="New Task" :ref="formId">
+    <div id="form">
+      <b-form @submit.prevent="onSubmit" @reset.prevent="onReset" v-if="show">
+        <b-form-group id="input-group-1" label="Title:" label-for="input-1">
+          <b-form-input
+            id="input-1"
+            v-model="form.title"
+            required
+            placeholder="Enter title"
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group id="input-group-2" label="Description:" label-for="textarea ">
+          <b-form-textarea
+            id="textarea"
+            v-model="form.description"
+            placeholder="Enter description"
+            rows="3"
+            max-rows="6"
+          ></b-form-textarea>
+        </b-form-group>
+        <b-form-group id="input-group-3" label="Point:" label-for="input-3">
+          <b-form-input
+            id="input-3"
+            v-model="form.point"
+            placeholder="Enter point"
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group id="input-group-4" label="PIC:" label-for="input-4">
+          <b-form-input
+            id="input-4"
+            v-model="form.pic"
+            placeholder="Enter PIC"
+          ></b-form-input>
+        </b-form-group>
 
-      <b-button style="margin:0 5px" type="submit" variant="success">Submit</b-button>
-      <b-button style="margin:0 5px" type="reset" variant="danger">Reset</b-button>
-    </b-form>
-  </div>
+        <b-button style="margin:0 5px" type="submit" variant="success">Submit</b-button>
+        <b-button style="margin:0 5px" type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </div>
+  </b-modal>
 </template>
 
 <script>
 import db from '@/api/firebase';
+import Swal from 'sweetalert2';
 
 export default {
   name: 'Form',
+  props: {
+    formId: String,
+  },
   data() {
     return {
       form: {
@@ -73,11 +79,26 @@ export default {
       })
         .then((docRef) => {
           console.log('Document written with ID: ', docRef.id);
+          Swal.fire({
+            position: 'center',
+            type: 'success',
+            title: 'Task Created',
+            showConfirmButton: false,
+            timer: 1500,
+          });
         })
         .catch((error) => {
           console.error('Error adding document: ', error);
+          Swal.fire({
+            position: 'center',
+            type: 'error',
+            title: '500 - Internal Server Error',
+            showConfirmButton: false,
+            timer: 1500,
+          });
         });
       this.onReset();
+      this.$refs[this.formId].hide();
     },
     onReset() {
       // Reset our form values
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,212 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container fluid class="bv-example-row">
+      <div id="cardsCategory">
+        <b-card-group deck>
+          <b-card
+            v-for="(card, index) in cardsCategory"
+            :key="index"
+            :border-variant="card.variant"
+            :header="card.header"
+            :header-bg-variant="card.variant"
+            header-text-variant="white"
+            align="center"
+          >
+            <dd-card-task
+              v-for="(task, index) in card.array"
+              @backLog="backLog"
+              @toDo="toDo"
+              @doing="doing"
+              @done="done"
+              @remove="remove"
+              :key="index"
+              :task="task.data()"
+              :id="task.id"
+            />
+          </b-card>
+        </b-card-group>
+      </div>
+    </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import ddCardTask from '@/components/CardTask.vue';
+
+import db from '@/api/firebase';
+
+import Swal from 'sweetalert2';
+
+const swalWithBootstrapButtons = Swal.mixin({
+  customClass: {
+    confirmButton: 'btn btn-success',
+    cancelButton: 'btn btn-danger',
+  },
+  buttonsStyling: false,
+});
 
 export default {
-  name: 'home',
+  name: 'Home',
   components: {
-    HelloWorld,
+    ddCardTask,
+  },
+  created() {
+    this.initiate();
+  },
+  data() {
+    return {
+      cardsCategory: [
+        {
+          header: 'Back Log',
+          variant: 'danger',
+          array: [],
+        },
+        {
+          header: 'To Do',
+          variant: 'warning',
+          array: [],
+        },
+        {
+          header: 'Doing',
+          variant: 'primary',
+          array: [],
+        },
+        {
+          header: 'Done',
+          variant: 'success',
+          array: [],
+        },
+      ],
+    };
+  },
+  methods: {
+    // onSubmit(form) {
+    //   this.cardsCategory[0].array.push(form);
+    // },
+    initiate() {
+      db.collection('tasks')
+        .orderBy('createdAt', 'desc')
+        .onSnapshot((querySnapshot) => {
+          console.log('Data changed');
+          /* eslint no-param-reassign: ["error", { "props": false }] */
+          this.cardsCategory.map((card) => {
+            card.array = [];
+            return card;
+          });
+          querySnapshot.forEach((doc) => {
+            // console.log(doc.id, doc.data());
+            switch (doc.data().status) {
+              case 'BackLog':
+                this.cardsCategory[0].array.push(doc);
+                break;
+              case 'ToDo':
+                this.cardsCategory[1].array.push(doc);
+                break;
+              case 'Doing':
+                this.cardsCategory[2].array.push(doc);
+                break;
+              case 'Done':
+                this.cardsCategory[3].array.push(doc);
+                break;
+              default:
+                break;
+            }
+          });
+        });
+    },
+    remove(id) {
+      swalWithBootstrapButtons.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel!',
+        reverseButtons: true,
+      }).then((result) => {
+        if (result.value) {
+          db.collection('tasks').doc(id).delete()
+            .then(() => {
+              // console.log('Document removed');
+              // this.initiate();
+              swalWithBootstrapButtons.fire(
+                'Deleted!',
+                'Your task has been deleted.',
+                'success',
+              );
+            })
+            .catch((error) => {
+              console.error('Error removing document: ', error);
+              Swal.fire({
+                position: 'center',
+                type: 'error',
+                title: '500 - Internal Server Error',
+                showConfirmButton: false,
+                timer: 1500,
+              });
+            });
+        } else if (
+          // Read more about handling dismissals
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Cancelled',
+            'Your task is safe :)',
+            'error',
+          );
+        }
+      });
+    },
+    backLog(id) {
+      db.collection('tasks').doc(id).set({
+        status: 'BackLog',
+      }, { merge: true })
+        .then(() => {
+          console.log('Document updated');
+          // this.initiate();
+        })
+        .catch((error) => {
+          console.error('Error updating document: ', error);
+        });
+    },
+    toDo(id) {
+      db.collection('tasks').doc(id).set({
+        status: 'ToDo',
+      }, { merge: true })
+        .then(() => {
+          console.log('Document updated');
+          // this.initiate();
+        })
+        .catch((error) => {
+          console.error('Error updating document: ', error);
+        });
+    },
+    doing(id) {
+      db.collection('tasks').doc(id).set({
+        status: 'Doing',
+      }, { merge: true })
+        .then(() => {
+          console.log('Document updated');
+          // this.initiate();
+        })
+        .catch((error) => {
+          console.error('Error updating document: ', error);
+        });
+    },
+    done(id) {
+      console.log('induk done');
+      db.collection('tasks').doc(id).set({
+        status: 'Done',
+      }, { merge: true })
+        .then(() => {
+          console.log('Document updated');
+          // this.initiate();
+        })
+        .catch((error) => {
+          console.error('Error updating document: ', error);
+        });
+    },
   },
 };
 </script>
